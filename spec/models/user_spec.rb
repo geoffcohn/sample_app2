ab44638f561e2b97ed4c43e013cd79a13c11999b
Geoff Cohn <geoffcohn@gmail.com>
2014-05-01 12:30:55 -0500

    up to 11.2

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,6 +19,14 @@ describe User do
   it { should respond_to(:admin) }
   it { should respond_to(:microposts) }
   it { should respond_to(:feed) }
+  it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
+  it { should respond_to(:following?) }
+  it { should respond_to(:follow!) }
+  it { should respond_to(:unfollow!) }
+  it { should respond_to(:reverse_relationships) }
+
+
 
 
   it { should be_valid }
@@ -175,5 +183,32 @@ describe User do
     end # status
 
   end #end micropost associations
+
+  describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }
+    before do
+      @user.save
+      @user.follow!(other_user)
+    end
+
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+
+    describe "followed user" do
+      subject { other_user }
+      its(:followers) { should include(@user) }
+    end # followed user
+
+    describe "and unfollowing" do
+      before { @user.unfollow!(other_user) }
+
+      it { should_not be_following(other_user) }
+      its(:followed_users) { should_not include(other_user) }
+    end# and unfollowing
+
+  end # describe following
 
 end # end class (user do)
